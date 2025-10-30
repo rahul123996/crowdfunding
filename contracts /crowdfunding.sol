4bb203584c193b32b878cc475626ce71ecdc553f
Rahul Prajapati <rahulprajapati25708@gmail.com>
2025-10-30 12:38:20 +0530

    Update crowdfunding.sol

--- contracts /crowdfunding.sol
+++ contracts /crowdfunding.sol
@@ -5,6 +5,8 @@ contract Crowdfunding {
     address public owner;
     uint256 public goal;
     uint256 public totalFunds;
+    bool public goalReached;
+    bool public goalFailed;
     mapping(address => uint256) public contributions;
 
     constructor(uint256 _goal) {
@@ -15,23 +17,42 @@ contract Crowdfunding {
     // Function 1: Contribute to the crowdfunding goal
     function contribute() external payable {
         require(msg.value > 0, "Contribution must be greater than 0");
-        require(totalFunds < goal, "Funding goal already reached");
+        require(!goalReached, "Funding goal already reached");
+        require(!goalFailed, "Campaign ended");
 
         contributions[msg.sender] += msg.value;
         totalFunds += msg.value;
+
+        if (totalFunds >= goal) {
+            goalReached = true;
+        }
     }
 
-    // Function 2: Withdraw funds (only if goal is reached)
+    // Function 2: Withdraw funds (only owner)
     function withdraw() external {
         require(msg.sender == owner, "Only owner can withdraw");
-        require(totalFunds >= goal, "Funding goal not yet reached");
+        require(goalReached, "Goal not reached yet");
 
         payable(owner).transfer(address(this).balance);
     }
 
-    // Optional Function 3: View current balance
+    // Function 3: View contract balance
     function getBalance() external view returns (uint256) {
         return address(this).balance;
     }
-}
 
+    // Function 4: Refund contributors if goal not reached
+    function refund() external {
+        require(!goalReached, "Goal was reached, cannot refund");
+        uint256 contributed = contributions[msg.sender];
+        require(contributed > 0, "No contributions to refund");
+
+        contributions[msg.sender] = 0;
+        payable(msg.sender).transfer(contributed);
+    }
+
+    // Function 5: Get individual contributor’s amount
+    function getContributorAmount(address contributor) external view returns (uint256) {
+        return contributions[contributor];
+    }
+}
